e6003cc24c641ec15818580d55d5c2f257f5038c
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-04-02 00:54:41 -0500

    Creation Player 2.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 canvas.width = innerWidth
-canvas.height = innerheight
+canvas.height = innerHeight
 class Player {
     constructor (){
         this.position = {
