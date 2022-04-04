f7829ffe9326ec3283f86f5b3c43e768f989ba04
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-04-03 20:46:59 -0500

    Gravity

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+const gravity = 0.5
 class Player {
     constructor (){
         this.position = {
             x:100,
             y:100
+        }
+        this.velocity = {
+            x:0,
+            y:1
         }
         this.width=30
         this.height=30
@@ -15,7 +21,21 @@ class Player {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y,this.width, this.height)
     }
+    update (){
+        this.draw()
+        this.position.y =+ this.velocity.y
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y = gravity
+        else this.velocity = 0
+    }
 }
 
 const player = new Player()
 
+function animate (){
+ requestAnimationFrame(animate)
+ console.log('go')
+ c.clearRect(0, 0, canvas.width,canvas.height)
+ player.update()
+}
+animate()
