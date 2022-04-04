773323d82a66772e4f05dbadf55dea8802b5a981
David Fernando Briceño Galindo <61285906+Dafbrig@users.noreply.github.com>
2022-04-03 20:54:27 -0500

    gravity update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ class Player {
         this.position.y =+ this.velocity.y
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
         this.velocity.y = gravity
-        else this.velocity = 0
+        else this.velocity.y = 0
     }
 }
 
